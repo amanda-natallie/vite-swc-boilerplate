1142b25e0c2107776345c7c2498e78751b8a8cf0
Amanda Natallie de Oliveira <amanda@vallorisolutions.com>
2024-04-17 14:26:59 -0300

    chore(architecture): add initial files, husky, eslint & prettier config

---
+++ .eslintrc.cjs
@@ -0,0 +1,85 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'react', 'prettier', 'react-hooks', 'import-helpers'],
+  rules: {
+    'prettier/prettier': ['error'],
+    'no-use-before-define': 0,
+    'space-before-function-paren': 0,
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        newlinesBetween: 'always',
+        groups: [
+          '/^react/',
+          'module',
+          '/^@/',
+          '/^components/',
+          '/^useCases/',
+          '/^services/',
+          '/^hooks/',
+          '/^utils/',
+          '/^stores/',
+          '/^domains/',
+          '/^assets/',
+          ['parent', 'sibling', 'index'],
+        ],
+        alphabetize: {
+          order: 'asc',
+          ignoreCase: true,
+        },
+      },
+    ],
+    'import/no-extraneous-dependencies': [
+      'warn',
+      {
+        devDependencies: true,
+      },
+    ],
+    'import/prefer-default-export': 0,
+    'import/no-unresolved': 0,
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        ts: 'never',
+        tsx: 'never',
+        js: 'never',
+      },
+    ],
+    'no-restricted-syntax': 0,
+    'no-param-reassign': [
+      'error',
+      {
+        props: false,
+      },
+    ],
+    'object-literal-sort-keys': 0,
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+      },
+    ],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/require-default-props': 'off',
+    'max-params': ['error', 3],
+    '@typescript-eslint/no-shadow': 'error',
+    'import/no-relative-parent-imports': 'error',
+    'logical-assignment-operators': 'error',
+  },
+};
