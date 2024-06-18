5a43dcd28c0a0b27ff50a6443c1a154bc85f9e32
Amanda Natallie de Oliveira <amanda@vallorisolutions.com>
2024-06-18 16:23:33 -0300

    chore(tests): fix things

--- run-tests.sh
+++ run-tests.sh
@@ -40,18 +40,22 @@ if [[ $1 != "-c" && $1 == *"src"* ]]; then
   FILE_NAME=$(basename $1)
   # Executa vitest apenas para o arquivo especificado
   echo -e "${BLUE}Running a single test file: $FILE_NAME${RESET}"
+  unset NODE_OPTIONS 
   vitest $MAXWORKERS_FLAG $COVERAGE_FLAG $1  --no-cache
 elif [[ $1 == "-w" && $2 == *"src"* ]]; then
   FILE_NAME=$(basename $2)
   # Verifica se está no modo --watch e se o último argumento contém "src" para rodar em modo watch
   echo -e "${BLUE}Watching a single test file: $FILE_NAME${RESET}"
+  unset NODE_OPTIONS 
   vitest $MAXWORKERS_FLAG --silent --watch $COVERAGE_FLAG $2  --no-cache
 elif [[ $1 == "-w" ]]; then
   # Verifica se está no modo --watch para personalizar a mensagem
   echo -e "${BLUE}Watching $COVERAGE_MESSAGE files${RESET}"
+  unset NODE_OPTIONS 
   vitest $MAXWORKERS_FLAG --no-cache $COVERAGE_FLAG --watch
 else
   # Executa vitest com o número de workers ajustado e a opção -c, se fornecida
   echo -e "${BLUE}Running $COVERAGE_MESSAGE with $NUM_WORKERS CPU cores${RESET}"
+  unset NODE_OPTIONS 
   vitest $COVERAGE_FLAG $MAXWORKERS_FLAG --no-cache --watch=false
 fi
